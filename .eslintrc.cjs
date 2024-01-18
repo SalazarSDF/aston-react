d9893b9473f46d1d2a221c4bb148e3e50d8a1c9b
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2024-01-18 11:01:16 -0500

    add: eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,41 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-console": ["error"],
+    curly: ["error", "all"],
+    "import/extensions": "off",
+    "import/prefer-default-export": "warn",
+    "no-unused-expressions": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always-and-inside-groups",
+      },
+    ],
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>Hello world</h1>
