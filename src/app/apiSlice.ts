2b3351254b2989efce7c41661a48cbc7c1f3cc01
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2024-01-23 08:58:35 -0500

    add: RTK query

--- src/app/apiSlice.ts
+++ src/app/apiSlice.ts
@@ -1,2 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { Recepie } from "../widgets/cards-list";
+type RecipiesData = {
+  recipes: Recepie[];
+};
 
+export const apiSlice = createApi({
+  // The cache reducer expects to be added at `state.api` (already default - this is optional)
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com" }),
+  tagTypes: ["Recipies"],
+  // The "endpoints" represent operations and requests for this server
+  endpoints: (builder) => ({
+    getInitalRecipies: builder.query<RecipiesData, void>({
+      query: () => "/recipes?limit=10&skip=10",
+    }),
+    searchRecipies: builder.query<RecipiesData, string>({
+      query: (query: string) => `recipes/search?q=${query}`,
+    }),
+  }),
+});
 
+// Export the auto-generated hook for the `getPosts` query endpoint
+export const { useGetInitalRecipiesQuery, useSearchRecipiesQuery } = apiSlice;
