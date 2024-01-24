7ae28437e5d3d54a8884a1438f6e0e10f9da5518
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2024-01-24 08:00:35 -0500

    fix: add useEffect dependency

--- src/entities/image-with-loader.tsx
+++ src/entities/image-with-loader.tsx
@@ -15,7 +15,7 @@ export default function ImageWithLoader({ src, alt }: Props) {
       setImageLoaded(true);
     };
     img.src = src;
-  });
+  }, [src]);
   if (!imageLoaded) {
     return <Spinner />;
   }
