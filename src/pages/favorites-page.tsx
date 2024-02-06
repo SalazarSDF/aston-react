214f42367752ced275d9ec8a9cf6aa6bc0aeff9c
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2024-02-06 04:47:14 -0500

    fix: redirect not auth users fix: initial cards render

--- src/pages/favorites-page.tsx
+++ src/pages/favorites-page.tsx
@@ -1,5 +1,18 @@
+import { useSelector } from "react-redux";
+
+import { Navigate } from "react-router-dom";
+
 import FavoritesList from "../widgets/favorites-list";
+
+import { getUserData } from "../features/users/userSlice";
+
+
 export default function Favorites() {
+  const user = useSelector(getUserData);
+
+  if (!user || !user.email) {
+    return <Navigate to="/sign-up" />;
+  }
   return (
     <div className="cards-list">
       <FavoritesList />
