d359d2715cc451c9643841f19ae4a45e1b1ebab7
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2024-02-07 10:35:22 -0500

    feat: react Suspense

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,33 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Header from "../entities/header";
-import Main from "../pages/main-page";
-import Favorites from "../pages/favorites-page";
 import "./index.css";
-import SignIn from "../pages/sign-in-page";
-import SignUpForm from "../pages/sign-up-page";
-import History from "../pages/history-page";
-import SingleCard from "../entities/card-single";
+
+import Header from "../entities/header";
+const Main = lazy(() => import("../pages/main-page"));
+const Favorites = lazy(() => import("../pages/favorites-page"));
+const SignIn = lazy(() => import("../pages/sign-in-page"));
+const SignUpForm = lazy(() => import("../pages/sign-up-page"));
+const History = lazy(() => import("../pages/history-page"));
+const SingleCard = lazy(() => import("../entities/card-single"));
+
+import Spinner from "../entities/spinner";
 
 function App() {
   return (
     <Suspense fallback={"loading..."}>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Main />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUpForm />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/history" element={<History />} />
-          <Route path="recipe/:recipeId" element={<SingleCard />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Main />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUpForm />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/history" element={<History />} />
+            <Route path="recipe/:recipeId" element={<SingleCard />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Suspense>
   );
 }
